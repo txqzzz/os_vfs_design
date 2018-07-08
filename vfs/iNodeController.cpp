e52d9ecdaf8da7d6a45db16557d3f42215ba2086
txqzzz <txqzzz@hotmail.com>
2018-07-08 13:52:34 +0800

    delete cmake git files

--- vfs/iNodeController.cpp
+++ vfs/iNodeController.cpp
@@ -2,34 +2,37 @@
 // Created by Xingqi Tang on 2018/7/5.
 //
 #include "iNodeController.h"
+
 bool iNodeController::write_iNode(iNode i_inode, fsController &fs_Controller) {
     this->inode = i_inode;
     char buf[BLOCK_SIZE];
-    memcpy(buf, (char *) &i_inode,BLOCK_SIZE);
+    memcpy(buf, (char *) &i_inode, BLOCK_SIZE);
     vhdController vhd_Controller;
     vhd_Controller.load_vhd();
-    vhd_Controller.write_vhd(buf,fs_Controller.get_next_free_list_index());
+    vhd_Controller.write_vhd(buf, fs_Controller.get_next_free_list_index());
     return true;
 }
 
 bool iNodeController::del_iNode(iNode i_Node) {
     int id;
-    id=i_Node.i_ino;
+    id = i_Node.i_ino;
     vhdController vhd_Controller;
     vhd_Controller.load_vhd();
     vhd_Controller.del_vhd(id);
     return true;
 }
+
 bool iNodeController::read_iNode(int id, iNode &i_Node) {
     char buf[BLOCK_SIZE];
     vhdController vhd_Controller;
     vhd_Controller.load_vhd();
-    vhd_Controller.read_vhd(buf,id);
-    memcpy((char*)&i_Node, buf, sizeof(buf));
+    vhd_Controller.read_vhd(buf, id);
+    memcpy((char *) &i_Node, buf, sizeof(buf));
     return true;
 }
-int  iNodeController::find_iNode_by_uid(int uid) {
-    for (int i=INODE_START;i<INODE_END;i++) {
+
+int iNodeController::find_iNode_by_uid(int uid) {
+    for (size_t i = INODE_START; i <= INODE_END; i++) {
         char buf[sizeof(this->inode)];
         vhdController vhd_Controller;
         vhd_Controller.load_vhd();
--- vfs/main.cpp
+++ vfs/main.cpp
@@ -9,14 +9,15 @@
 using namespace std;
 
 int main() {
+    /* Test Utils */
     vhdController _vhd_;
     fsController _fs_;
     iNodeController my_iNode;
     _fs_.init_iNode_free_list();
-    if(TestController::testvhdController(_vhd_))
-        cout<<"vhdcontroller is right"<<endl;
-    if (TestController::testiNodeController(my_iNode,_fs_))
-        cout<<"iNode is right"<<endl;
+    if (TestController::testvhdController(_vhd_))
+        cout << "[ok] vhdController Test Checked." << endl;
+    if (TestController::testiNodeController(my_iNode, _fs_))
+        cout << "[ok] iNode Test Checked." << endl;
 
     return 0;
 }
